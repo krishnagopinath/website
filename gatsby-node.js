bf7bce122b56c8a5bf59f21ac3b6962b03509fe2
krishna gopinath <hpkrishna07@gmail.com>
2020-01-16 20:59:38 +0530

    Disable sourcemaps in production mode

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions, stage }) => {
+  // If production JavaScript and CSS build
+  if (stage === "build-javascript") {
+    // Turn off source maps
+    actions.setWebpackConfig({
+      devtool: false,
+    })
+  }
+}
