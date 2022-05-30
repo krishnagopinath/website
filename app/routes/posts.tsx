f9505ca942544945c52dacc157843d17474e464e
krishna gopinath <hpkrishna07@gmail.com>
2022-05-30 18:07:46 -0400

    chore: make blog page look better

--- app/routes/posts.tsx
+++ app/routes/posts.tsx
@@ -14,7 +14,7 @@ export const meta: MetaFunction = () => {
 export default function PostsPage() {
   return (
     <>
-      <SiteHeader />
+      <SiteHeader hideHeader={true} />
       <div className="flex flex-col flex-wrap">
         <div className="prose">
           <Outlet />
