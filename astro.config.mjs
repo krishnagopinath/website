f500fc0707ca585d4c459ba7c4fc4804ad96231e
Krishna Gopinath <hpkrishna07@gmail.com>
2023-09-29 00:03:20 -0500

    chore: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false }), mdx()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+  ],
+  output: "server",
+  adapter: netlify(),
 });
