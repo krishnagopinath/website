024d314824e72d2e444ddea4b5c49f62aa6aa118
Krishna Gopinath <hpkrishna07@gmail.com>
2023-10-01 19:38:08 -0500

    feat: add resume section

---
+++ src/utils/dates.ts
@@ -0,0 +1,15 @@
+import format from "date-fns/format"
+
+/**
+ * Given a date and a supported date format, formats the date
+ */
+export const formatDate = (dateString: string, dateFormat: Formats) => format(new Date(dateString), dateFormat)
+
+/**
+ * Enum for 'supported' date formats
+ */
+export enum Formats {
+  Year = "uuuu",
+  MonthAndYear = "MMM uuuu",
+  DayMonthYear = "d MMM, uuuu"
+}
